b5182b5eb8b111a35157b38d2c2a76800da07eab
Vandenborne Stefan <stefan@illutek.be>
2016-02-01 14:36:19 +0100

    Footer opmaak

--- templates/partials/footer.inc.php
+++ templates/partials/footer.inc.php
@@ -6,6 +6,20 @@
  * Time: 14:40
  */
 ?>
-<footer class="col-md-12 footer-wrap">
-    <p>TRADE BRUSSELS<br> ATOMIUMSQUARE 1 B91 | 1020 BRUSSELS | TEL : 02/478 50 50</p>
+<footer>
+    <div class="container">
+        <div class="col-md-12 footer-wrap">
+            <div class="col-md-6">
+                <p><a href="<?php print base_path() ?>legal">Terms and Conditions</a>  |
+                    <a href="<?php print base_path() ?>privacy">Privacy</a> |
+                    <a href="<?php print base_path() ?>cookies">Cookies</a></p>
+                <p>&copy; 2015 - <?php echo date("Y"); ?> Chartage</p>
+                <p>TRADE BRUSSELS<br> ATOMIUMSQUARE 1 B91 | 1020 BRUSSELS | TEL : 02/478 50 50</p>
+            </div>
+            <div class="col-md-6 mail-chimp">
+                <!-- Begin MailChimp Signup Form -->
+                <a href="http://eepurl.com/Iu6Kj" target="_blank"><i class="fa fa-envelope-o fa-lg"></i>Subscribe to our mailing list</a>
+            </div>
+        </div>
+    </div>
 </footer>
