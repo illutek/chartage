a04dc2c2e9cd2bd0f09e92b4019d728cdf6836fc
Vandenborne Stefan <stefan@illutek.be>
2017-09-27 21:02:38 +0200

    comment update

--- templates/partials/more-info-btn.inc.php
+++ templates/partials/more-info-btn.inc.php
@@ -4,7 +4,9 @@
  * User: Stefan
  * Date: 24/01/2016
  * Time: 11:19
+ * $model is een variabele van ubercart zelf
  */
+
 $textMoreInfo = t("Get info about this product");
 ?>
 <!-- link naar meer info form hier node/6 -->
