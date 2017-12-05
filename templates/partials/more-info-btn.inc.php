dccbabc519e0114bbc4a380c7e1e9e1a7f9df651
VDB Stefan <stefanvandenborne@gmail.com>
2017-12-05 15:50:05 +0100

    to gulp and node--product to BEM

--- templates/partials/more-info-btn.inc.php
+++ templates/partials/more-info-btn.inc.php
@@ -10,10 +10,12 @@
 $textMoreInfo = t("Get info about this product");
 ?>
 <!-- link naar meer info form hier node/6 -->
-<div class="col-md-7">
+
+<div class="product-info__btn product-info__btn--more-info">
     <a href="<?php print base_path() ?>node/6?product=<?php print_r($model) ?>">
         <button type="button" class="btn btn-default">
             <i class="fa fa-info fa-lg"></i><?php print $textMoreInfo; ?>
         </button>
     </a>
 </div>
+
--- templates/partials/sets-button.inc.php
+++ templates/partials/sets-button.inc.php
@@ -14,13 +14,16 @@ $textSet = t("Matching Jewelry");
 if ($value):
     $contentSet = $content['field_set']['#items'][0]['taxonomy_term']->name;
     ?>
-    <div class="product-set col-md-5">
-        <a href="<?php print base_path() ?>sets/<?php print $contentSet; ?>">
-            <button type="button" class="btn btn-default">
-                <i class="fa fa-cubes fa-lg"></i><?php print $textSet; ?>
-            </button>
-        </a>
-    </div>
+
+        <div class="product-info__btn product-info__btn--set">
+            <a href="<?php print base_path() ?>sets/<?php print $contentSet; ?>">
+                <button type="button" class="btn btn-default">
+                    <i class="fa fa-cubes fa-lg"></i><?php print $textSet; ?>
+                </button>
+            </a>
+        </div>
+
+
 <?php else:
     print '';
 endif;
